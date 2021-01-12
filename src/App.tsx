da756271c7b668078580518abe9fe4dbd6663f5f
Fabiano Moraes <fabiano.m.moraes@gmail.com>
2021-01-12 15:50:04 -0300

    Simple input implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App: React.FC = () => <div>Zero config</div>;
+const App: React.FC = () => {
+  const [text, setText] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <Input value={text} onChange={handleChange}>
+        Input:
+      </Input>
+      <p>You typed: {text || '...'}</p>
+    </div>
+  );
+};
+
+interface InputProps {
+  children: React.ReactNode;
+  value: string;
+  onChange(event: React.ChangeEvent<HTMLInputElement>): void;
+}
+
+const Input = ({ children, value, onChange }: InputProps) => {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input id="search" type="text" value={value} onChange={onChange} />
+    </div>
+  );
+};
 
 export default App;
