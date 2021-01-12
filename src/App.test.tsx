ea96514210e12f83a5ba498469cb79226551b0b5
Fabiano Moraes <fabiano.m.moraes@gmail.com>
2021-01-12 16:10:17 -0300

    Wip

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('When everthing is OK', () => {
   test('should render the App component without crashing', () => {
     render(<App />);
+    screen.debug();
+  });
+
+  test('should render Input label text and Input children', () => {
+    render(<App />);
+    screen.getByText('Input:');
+    expect(screen.getByText('Input:')).toBeInTheDocument();
   });
 });
